b74d5d2f45887568071e4dfb8fd3843eed103bd7
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-02-27 22:47:55 -0500

    homework

---
+++ file.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+// fs.readFile('./docs/blog.txt', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+
+// fs.writeFile('./docs/blog.txt', "Here Comes A New Challenger", ()=> {
+//     console.log('file was written');
+// });
+// fs.writeFile('./docs/blog2.txt', 'you win perfect', () => {
+//     console.log ('file was written');
+// })
+
+// if (!fs.existsSync('./assets')) {
+//     fs.mkdir('./assets', (err) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log('folder created');
+//     });
+// } else {
+//     fs.rmdir('./assets', (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         console.log('folder deleted');
+//     })
+// }
+
+// if (fs.existsSync('./docs/deleteme.txt')) {
+//     fs.unlink('./docs/deleteme.txt', (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         console.log('file delete');
+//     })
+// }
---
+++ people.js
@@ -0,0 +1,7 @@
+const people = ['Ryu','Ken','Chun-Li','Akuma','Juri']
+const ages = [32, 31, 28, 35, 27];
+console.log(people);
+
+ module.exports = {
+    people, ages
+ }
