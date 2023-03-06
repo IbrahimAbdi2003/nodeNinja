432cc20142e2b6eb00cc67ac1302ef1dcb986745
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-03-06 09:11:10 -0500

    mongoose make me go goose

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
-app.listen(3000);
+const dbURI = "mongodb+srv://Ibrahim2003:password1234@node-ninja.cmfpray.mongodb.net/test";
 
-// app.get('/', (req, res) => {
-//     // res.send('<p>homepage</p>');
-// });
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+app.set('view engine', 'ejs');
+
+// use('mongodbVSCodePlaygroundDB');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 app.get('/', (req, res) => {
-    // res.send('<p>homepage</p>');
-    res.sendFile('./view/index.html', {root: __dirname});
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>');
-    res.sendFile('./view/about.html', {root: __dirname});
+  res.render('about', { title: 'About' });
 });
 
-app.get('/about-us', (req, res) => {
-    // res.send('<p>about page</p>');
-    res.redirect('/about');
-});
+app.use('/blogs', blogRoutes);
 
-app.use((req,res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
